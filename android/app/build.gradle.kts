0196c015ac9bb16332a843a4dc6114931b9e4af2
youssefescobar <coolgoot123@gmail.com>
2026-03-07 11:08:59 +0200

    u need this to build gang

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("androidx.core:core-telecom:1.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
 }
