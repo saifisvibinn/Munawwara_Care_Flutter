04ba1bd50a23dc10785e0fcc26f84803098fa820
saifisvibin <saifsaeedawad.98@gmail.com>
2026-06-05 16:32:58 +0300

    feat: implement pilgrim profile bottom sheet with quick action controls for messaging and calling

--- lib/features/moderator/widgets/pilgrim_profile_sheet.dart
+++ lib/features/moderator/widgets/pilgrim_profile_sheet.dart
@@ -1,4 +1,4 @@
-import 'package:easy_localization/easy_localization.dart';
+EFEWFWEFWEFWEF34import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
